e7b040776dde2c7029744246a76f032707e7049d
TheButterbrotMan <64548098+TheButterbrotMan@users.noreply.github.com>
2022-12-09 22:30:01 +0100

    Initial commit

---
+++ src/main/java/net/thebutterbrotman/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.thebutterbrotman.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
